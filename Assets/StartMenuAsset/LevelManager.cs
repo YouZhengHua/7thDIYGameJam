eaf6a541eedfcf8656d5c9ca71757299909cf9fc
YunChienLo <curtis0226@gmail.com>
2023-07-08 15:05:27 +0800

    UpgradeUI, levelManager

---
+++ Assets/StartMenuAsset/LevelManager.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private List<LevelSO> _levels = new List<LevelSO>();
+
+
+}
