269b1df287217794cea24412090bbd27cd8960ae
zhenghua <aaazzzz168@gmail.com>
2023-07-26 01:44:59 +0800

    fix: 修正盾牌自動銷毀導致的錯誤訊息

--- Assets/Scripts/Game/BulletController.cs
+++ Assets/Scripts/Game/BulletController.cs
@@ -9,21 +9,21 @@ namespace Scripts.Game
         private float _targetX = 0f;
         private float _targetY = 0f;
         private float _flySpeed = 0f;
+        private bool _havePenetrationLimit = false;
         private int _penetrationCount = 1;
         private Rigidbody2D _rigidbody2D;
         [SerializeField, Header("飛行距離上限")]
         private float _maxRange = 15f;
         [SerializeField, Header("目標碰撞圖層")]
         private LayerMask _targetLayer;
-        private Transform _playerContainer;
         private float _damage;
         private UnityEvent<Collider2D> _onHitEvenet = new UnityEvent<Collider2D>();
         private Vector3 _startPostion;
+        private bool _needAutoDestory = true;
 
         private void Awake()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
-            _playerContainer = GameObject.Find("PlayerContainer").GetComponent<Transform>();
         }
 
         private void Update()
@@ -32,7 +32,7 @@ namespace Scripts.Game
             {
                 _rigidbody2D.velocity = Vector2.ClampMagnitude(new Vector2(_targetX, _targetY), 1f) * _flySpeed;
             }
-            if ((transform.position - _startPostion).magnitude > _maxRange)
+            if (_needAutoDestory && (transform.position - _startPostion).magnitude > _maxRange)
             {
                 Destroy(this.gameObject);
             }
@@ -43,7 +43,7 @@ namespace Scripts.Game
             if ((1 << collision.gameObject.layer | _targetLayer) == _targetLayer)
             {
                 OnHitEvent?.Invoke(collision);
-                if (_penetrationCount > 0)
+                if (_penetrationCount > 0 && _havePenetrationLimit)
                 {
                     _penetrationCount--;
                     if (_penetrationCount == 0)
@@ -61,6 +61,8 @@ namespace Scripts.Game
             _damage = damage;
             _startPostion = transform.position;
         }
+
+        public bool NeedAutoDestory { set => _needAutoDestory = value; }
 
         public float Damage { get => _damage; }
         public UnityEvent<Collider2D> OnHitEvent { get => _onHitEvenet; }
