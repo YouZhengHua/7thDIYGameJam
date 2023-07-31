195e949bb4479689fc43f71014b9dbbcdf36c0bd
zhenghua <aaazzzz168@gmail.com>
2023-07-31 20:34:21 +0800

    feat: 更新按鈕音效

---
+++ Assets/Scripts/Tool/CommonButtonController.cs
@@ -0,0 +1,16 @@
+﻿using Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonButtonController : MonoBehaviour
+{
+    [Header("點擊時播放的音效")]
+    public AudioClip ClickSound;
+    public void PlayClickSound(float extendVolume = 0f)
+    {
+        if (ClickSound == null)
+            return;
+        AudioController.Instance.PlayEffect(ClickSound, extendVolume);
+    }
+}
